733009bf2a25ec1bdc5bf8e8edc501dbea902349
vincentzhang <vincentzhang@ea.com>
2019-01-02 17:23:44 +0800

    Added TCPServer part

---
+++ NetworkTransfer/TCPServer.py
@@ -0,0 +1,64 @@
+import socket
+import threading
+
+
+class TCPServer:
+    def __init__(self):
+        self.bind_ip = '0.0.0.0'
+        self.bind_port = 9999
+        self.buf_map = {}
+
+    def get_remain_buf(self, client_socket):
+        if client_socket not in self.buf_map:
+            self.buf_map[client_socket] = ""
+        return self.buf_map[client_socket]
+
+    def handle_client_connection(self, client_socket):
+        is_connection_alive = True
+        while is_connection_alive:
+            request_line = client_socket.recv(1024)
+            remain_buf = self.get_remain_buf(client_socket)
+            remain_buf = remain_buf + request_line
+
+            # search for line_breaks
+            line_break_idx = remain_buf.find("\n")
+
+            while line_break_idx != -1:
+                command = remain_buf[0:line_break_idx]
+                remain_buf = remain_buf[line_break_idx + 1:]
+                command = command.rstrip()
+                if len(command) > 0:
+                    if command.lower() == "bye" or command.lower() == "quit":
+                        is_connection_alive = False
+                    else:
+                        self.handle_client_command(command)
+                line_break_idx = remain_buf.find("\n")
+            self.buf_map[client_socket] = remain_buf
+
+        client_socket.close()
+        del self.buf_map[client_socket]
+        print "Connection broken, client thread quit!"
+
+    def handle_client_command(self, command_line):
+        print "Got command:%s" % command_line
+
+    def create_server(self):
+        print "Creating socket"
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print "Binding to %s:%d" % (self.bind_ip, self.bind_port)
+        self.server.bind((self.bind_ip, self.bind_port))
+        print "Listening"
+        self.server.listen(5)  # max backlog of connections
+
+        while True:
+            client_sock, address = self.server.accept()
+            print 'Accepted connection from {}:{}'.format(address[0], address[1])
+            client_handler = threading.Thread(
+                target=self.handle_client_connection,
+                args=(client_sock,)
+            )
+            client_handler.start()
+
+
+tcpServer = TCPServer()
+tcpServer.create_server()
