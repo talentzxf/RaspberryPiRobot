cb736843d13a700fad82486b879ca8775acb262a
vincentzhang <vincentzhang@ea.com>
2019-01-08 15:27:47 +0800

    Data binding is working

--- AndroidControl/app/src/main/java/com/vincentzhang/robotcontrol/MainActivity.java
+++ AndroidControl/app/src/main/java/com/vincentzhang/robotcontrol/MainActivity.java
@@ -1,13 +1,19 @@
 package com.vincentzhang.robotcontrol;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.vincentzhang.robotcontrol.databinding.ActivityMainBinding;
+import com.vincentzhang.robotcontrol.model.ControllerModel;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        // setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+         binding.setControllerModel(new ControllerModel());
     }
 }
---
+++ AndroidControl/app/src/main/java/com/vincentzhang/robotcontrol/model/ControllerModel.java
@@ -0,0 +1,17 @@
+package com.vincentzhang.robotcontrol.model;
+
+/**
+ * Created by VincentZhang on 1/8/2019.
+ */
+
+public class ControllerModel {
+    private String msg = "Hello Data Binding";
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+}
